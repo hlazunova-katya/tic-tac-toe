69428602a25a3cd13d833bcca830c8099627d3d8
hlazunova-katya <katrin_glazunova@mail.ru>
2017-09-18 17:11:39 +0300

    nextTurn and getCurPlayer implemented

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,18 @@
 class TicTacToe {
     constructor() {
-
+        this.cur_player = 'x';
+        this.field = [,];
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.cur_player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.field[rowIndex][columnIndex]===null){
+            this.field[rowIndex][columnIndex]=this.cur_player;
+        }
+        (this.cur_player==='x') ? this.cur_player = 'o' : this.cur_player = 'x';
     }
 
     isFinished() {
