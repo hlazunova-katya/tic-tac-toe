a27de6f1960c4d99a884a8b6bff1dd79b438cc74
hlazunova-katya <katrin_glazunova@mail.ru>
2017-09-19 18:00:20 +0300

    methods completed

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,7 +1,7 @@
 class TicTacToe {
     constructor() {
         this.cur_player = 'x';
-        this.field = [,];
+        this.field = [[null,null,null],[null,null,null],[null,null,null] ];
     }
 
     getCurrentPlayerSymbol() {
@@ -11,28 +11,56 @@ class TicTacToe {
     nextTurn(rowIndex, columnIndex) {
         if (this.field[rowIndex][columnIndex]===null){
             this.field[rowIndex][columnIndex]=this.cur_player;
+            //this.cur_player === 'x' ? this.cur_player = 'o' : this.cur_player = 'x';
+            this.cur_player = (this.cur_player === 'x') ? 'o' : 'x';
         }
-        (this.cur_player==='x') ? this.cur_player = 'o' : this.cur_player = 'x';
     }
 
     isFinished() {
-
+       if (this.isDraw()||this.getWinner()!==null) return true;
+       else return false;
     }
 
     getWinner() {
-
+        for (let i=0; i<3; i++){
+            if (
+                this.field[i][0]!==null
+                && this.field[i][0]===this.field[i][1]
+                && this.field[i][0]===this.field[i][2]
+            )
+                return this.field[i][0];
+            else  if (
+                this.field[0][i]!==null
+                && this.field[0][i]===this.field[1][i]
+                && this.field[0][i]===this.field[2][i]
+            )
+                return this.field[0][i];
+        }
+        //проверка диагоналей
+        return (this.field[0][0] !== null
+            && this.field[0][0] === this.field[1][1]
+            && this.field[0][0] === this.field[2][2])
+            ||
+            (this.field[0][2] !== null
+            && this.field[0][2] === this.field[1][1]
+            && this.field[0][2] === this.field[2][0])
+            ? this.field[1][1] : null;
     }
 
     noMoreTurns() {
-
+        for (let i = 0; i < 3; i++) {
+            if (this.field[i].includes(null))
+                return false;
+        }
+        return true;
     }
 
     isDraw() {
-
+       return this.noMoreTurns()&&(this.getWinner()===null);
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
